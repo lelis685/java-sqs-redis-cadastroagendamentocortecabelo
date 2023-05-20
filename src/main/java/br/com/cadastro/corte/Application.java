d302b05aeabd2b7b607c9c13688c94490c833210
lelis <lelis685@gmail.com>
2023-05-20 09:25:21 -0300

    config docker-compose

--- src/main/java/br/com/cadastro/corte/Application.java
+++ src/main/java/br/com/cadastro/corte/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroagendamentocortecabeloApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroagendamentocortecabeloApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
