4097b1449fd126955abfee4cce935d14e88c1e30
lelis <lelis685@gmail.com>
2023-05-20 07:38:12 -0300

    config inicial

---
+++ src/main/java/br/com/cadastro/corte/CadastroagendamentocortecabeloApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.corte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroagendamentocortecabeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroagendamentocortecabeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastro/corte/CadastroagendamentocortecabeloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.corte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroagendamentocortecabeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
